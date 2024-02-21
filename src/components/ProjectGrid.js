dfbe9070a9b002a81ce68379b14d8101a67ff068
Patticatti <xpatticattix@gmail.com>
2024-02-21 01:57:22 -0800

    Created grid with Project components

--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -1,26 +1,45 @@
-import { Text, Grid } from '@chakra-ui/react';
+import { Text, Stack, Box, Grid } from '@chakra-ui/react';
+
+
+const projects = [
+    {
+      title: "Storiator",
+      tags: ["volunteer", "ux design", "frontend"],
+      image: "./public/logo192.png"
+    },
+    {
+      title: "Project 2",
+      tags: ["backend", "database", "API"],
+      image: "./public/logo192.png"
+    },
+    // Add more projects as needed
+];
+
+function ProjectComponent({ title, tags, image }){
+    return(
+        <Stack marginBlock="2rem">
+            <Box height="300px" bgImage={image} boxShadow="0 20px 20px rgba(0, 0, 0, 0.25)"/>
+            <Text>
+        {tags.map((tag, index) => (
+          <span key={index}>
+            {tag}
+            {index !== tags.length - 1 && <>&nbsp;&bull;&nbsp;</>}
+          </span>
+        ))}
+      </Text>
+
+        </Stack>
+
+
+    );
+}
 
 function ProjectGrid() {
   return (
-    <Grid templateColumns="1fr 1fr" gap={4}>
-      {/* First column */}
-      <Text fontFamily="Outfit" fontSize="1rem" padding="1rem">
-        patti tani
-      </Text>
-
-      {/* Second column */}
-      <Text fontFamily="Outfit" fontSize="1rem" padding="1rem">
-        work
-      </Text>
-      <Text fontFamily="Outfit" fontSize="1rem" padding="1rem">
-        play
-      </Text>
-      <Text fontFamily="Outfit" fontSize="1rem" padding="1rem">
-        about
-      </Text>
-      <Text fontFamily="Outfit" fontSize="1rem" padding="1rem" color="primary">
-        resume
-      </Text>
+    <Grid templateColumns="1fr 1fr" padding="1rem" gap={4}>
+      {projects.map((project, index) => (
+        <ProjectComponent key={index} {...project} />
+      ))}
     </Grid>
   );
 }
